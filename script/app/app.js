c0b960e5970614cdd3353e4f606259ffc00f7097
danillewin <danillewin@gmail.com>
2015-07-22 23:38:55 +0300

    Auto-generated commit

--- script/app/app.js
+++
@@ -1,6 +0,0 @@
-app = {};
-app.vm = {};
-
-app.getRandomInt = function (min, max){
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
--- script/app/table.js
+++
@@ -1,9 +0,0 @@
-(function (app) {
-    
-    var flightsAPI = new app.Flights();
-        
-    this.flights = flightsAPI.fetch(30);
-    
-    ko.applyBindings(this);
-    
-})(window.app)
